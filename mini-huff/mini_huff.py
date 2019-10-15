5c19951ccf5a9071b94689775e6ae57be998c8e3
Gunvir Ranu <g1ranu@gmail.com>
2019-10-15 12:37:00 -0400

    Refactor mini-huff for shorter and nicer code
    * Check for empty input for compress and decompress
    * Refactor argument parsing slightly
    * Equivalent length expressions so a few more variables can be inlined

--- mini-huff/mini_huff.py
+++ mini-huff/mini_huff.py
@@ -7,6 +7,8 @@ from struct import pack, unpack, iter_unpack
 
 
 def compress(text: bytes) -> bytes:
+    if not text:
+        return bytes()
     weights = {}
     for let in text:
         weights[let] = weights.get(let, 0) + 1
@@ -22,20 +24,26 @@ def compress(text: bytes) -> bytes:
             stak.extend([(node[0][i], node[1] + str(i)) for i in range(2)])
     bits = "".join(codes[let] for let in text)
     compressed_text = bytes(int(bits[i : i + 8], 2) for i in range(0, len(bits), 8))
-    alt_codes = [(int(code, 2) | (1 << len(code)), let) for let, code in codes.items()]
-    serial_codes = pack(">" + "LB" * len(alt_codes), *(i for pair in alt_codes for i in pair))
-    return bytes([len(bits) % 8]) + pack(">L", len(serial_codes)) + serial_codes + compressed_text
+    alt_codes = ((int(code, 2) | (1 << len(code)), let) for let, code in codes.items())
+    serial_codes = pack(">" + "LB" * len(codes), *(i for pair in alt_codes for i in pair))
+    return b"".join((
+        bytes((len(bits) % 8,)),
+        pack(">L", len(codes) * (1 + 4)),
+        serial_codes,
+        compressed_text,
+    ))
 
 
 def decompress(compressed: bytes) -> bytearray:
-    unpad = compressed[0]
+    if not compressed:
+        return bytearray()
+    last_bits = (compressed[0] - 1) % 8
     code_len = unpack(">L", compressed[1:5])[0]
-    codes = {code: let for code, let in iter_unpack(">LB", compressed[5 : 5 + code_len])}
+    codes = dict(iter_unpack(">LB", compressed[5 : 5 + code_len]))
     compressed = memoryview(compressed)[5 + code_len :]
     text, cur = bytearray(), 1
     for i, byte in enumerate(compressed):
-        # TODO: Slightly faster if the conditional in the range is removed
-        for dig in range(7 if i + 1 != len(compressed) else (unpad - 1) % 8, -1, -1):
+        for dig in range(7 if i + 1 != len(compressed) else last_bits, -1, -1):
             cur = (cur << 1) | ((byte >> dig) & 1)
             tmp = codes.get(cur, None)
             if tmp is not None:
@@ -49,18 +57,16 @@ USAGE_TEXT = "Usage: mini_huff.py {compress|decompress} <infile> <outfile>"
 if __name__ == "__main__":
     if len(argv) != 4:
         exit("Invalid arguments.\n" + USAGE_TEXT)
-    if argv[1] not in ("compress", "decompress"):
+    funcs = {"compress": compress, "decompress": decompress}
+    if argv[1] not in funcs:
         exit("Invalid operation.\n" + USAGE_TEXT)
     with open(argv[2], "rb") as f:
         in_bytes = f.read()
-    if argv[1] == "compress":
-        out_bytes = compress(in_bytes)
-    elif argv[1] == "decompress":
-        out_bytes = decompress(in_bytes)
+    out_bytes = funcs[argv[1]](in_bytes)
     print("In: {}, Out: {}, Ratio: {:.2f}%".format(
             len(in_bytes),
             len(out_bytes),
-            100 * len(out_bytes) / len(in_bytes)
+            100 * (len(out_bytes) / len(in_bytes) if in_bytes else 1)
         )
     )
     with open(argv[3], "wb") as f:
